a8874dbc72285b02ebfc7afc9045c911093eab84
Keat <keatmin3@gmail.com>
2017-03-24 02:23:23 +0000

    Added language detector

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,4 +5,13 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    words = text.split()
+    detectedlang = ''
+    most_matches = 0
+    for lang in languages: 
+        matches = [word for word in words if word.lower() in lang['common_words']]
+        if len(matches) > most_matches:
+            detectedlang = lang['name']
+            most_matches = len(matches)
+    return detectedlang
+        
